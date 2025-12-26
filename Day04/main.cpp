342fb6ce8a899e1900edb3e3b102d47fcc200ab6
SvenFilipcic <154909686+SvenFilipcic@users.noreply.github.com>
2025-12-26 20:00:01 +0100

    Add files via upload

---
+++ Day04/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const vector<pair<int, int>> sosedi = {
+    {-1, -1}, {-1, 0}, {-1, 1},
+    {0, -1},           {0, 1},
+    {1, -1},  {1, 0},  {1, 1}
+};
+
+int main() {
+    ifstream file("input.txt");
+    vector<string> grid;
+    string line;
+
+    while (getline(file, line)) {
+        grid.push_back(line);
+    }
+
+    int counter = 0;
+
+    for (int i = 0; i < grid.size(); ++i) { 
+        for (int j = 0; j < grid[0].size(); ++j) { 
+            
+            if (grid[i][j] == '@') {
+                int sosednja_rollica = 0;
+                
+                for (const auto& offset : sosedi) {
+                    int ni = i + offset.first;
+                    int nj = j + offset.second;
+
+                    if (ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size()) {
+                        if (grid[ni][nj] == '@') {
+                            sosednja_rollica++;
+                        }
+                    }
+                }
+                if (sosednja_rollica < 4) {
+                    counter++;
+                }
+            }
+        }
+    }
+    cout << counter << endl;
+
+    return 0;
+}
